f30c401823d15d361abe9587071890226e2b857d
Shaurya Chandra <chandrashaurya70@gmail.com>
2026-05-11 19:15:42 +0530

    Add Day3 Python files

---
+++ Day3/whileloop.py
@@ -0,0 +1,6 @@
+n = 1
+a = int(input("input value: "))
+
+while n <= 10:
+    print(a, "x", n, "=", a*n)
+    n += 1
---
+++ Day3/whiletrue.py
@@ -0,0 +1,11 @@
+while True:
+
+    num1 = int(input("enter num1: "))
+    num2 = int(input("enter num2: "))
+
+    print(num1 + num2)
+
+    repeat = input("do you want to stop the program: ")
+    if repeat == "yes":
+        break
+
