54921b23252fdf6235e7fffdd718804c809c9963
Shaurya Chandra <chandrashaurya70@gmail.com>
2026-05-21 17:40:05 +0530

    Update Day8 files

--- Day8/array.py
+++ Day8/array.py
@@ -8,3 +8,13 @@ print(arr)
 
 # Slicing
 print(arr[0:3])
+
+# More functions:
+"""
+a.shape : array dimensions
+len(a) : length of array
+b.ndim : number of array dimensions
+e.size : number of array elements
+b.dtype : data type of array elements
+b.astype(int) : convert an array to a different type
+"""
